d9603153a8c71d57489aa3e0fccbcac8f5e913ed
Gitrepository <gitrepository001@gmail.com>
2021-03-03 09:41:41 +0530

    OOPs Related commands first commit

---
+++ OOPS.ps1
@@ -0,0 +1,9 @@
+﻿#Get-ExecutionPolicy
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+#Demo on the basics of the variables in PowerShell
+
+#example = 1
+$var1 = 777
+$var2 = 333
+$var3 = $var1 + $var2
+$var3
