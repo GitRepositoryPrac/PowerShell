9193a6f40f48a84bd85c2d1c1d93749b891adac7
Gitrepository <gitrepository001@gmail.com>
2021-03-03 11:17:44 +0530

    OOPs Related commands Array commit

--- OOPS.ps1
+++ OOPS.ps1
@@ -12,8 +12,8 @@ $var3
 #Example 2
 $var1 = "Complete"
 $var2 = "Ram"
-$var3 = $var1 + $var2
-$var3
+$var4 = $var1 + $var2
+$var4
 #Here Getting Error
 
 PS C:\WINDOWS\system32> $myname = "raj" #Declared the variables
@@ -26,3 +26,18 @@ S C:\WINDOWS\system32> $myname.GetType()
 IsPublic IsSerial Name                                     BaseType                                          
 -------- -------- ----                                     --------                                          
 True     True     String                                   System.Object                                     
+
+### Arrays are working on PowerShell 7.2.0 version
+### i' practiced PowerShell 7.2.0 version
+#### Array Declaring Arrys ####
+cls
+$luns = Import-Excel ~\Documents\WindowsPowerShell\Imports\LUN_Import.xlsx
+$arr1 = @()
+#Declarations of an Emppty Array
+$MyArray = @() # Array it will starts with on "@() symbols
+#Declaring an array with a set of values
+$ProdWebservers = @("Webserver1" , "Webserver2" , "Webserver3" , "Webserver4", "Webserver5")
+Write-Host $ProdWebservers
+#Declaring an array with a set of values
+$ProdDBservers = @("DBserver1" ,"DBserver2", "DBserver3", "DBserver4", "DBserver5")
+Write-Host $ProdDBservers
