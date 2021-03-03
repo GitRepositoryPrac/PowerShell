10c195916708acd3459c9f3229363a5e344ec508
Gitrepository <gitrepository001@gmail.com>
2021-03-03 07:12:41 +0530

    this commands are related to the POWERSHELL

---
+++ firstpracices.ps1
@@ -0,0 +1,38 @@
+﻿#This is program helps us in understanding what are cmdkets and how to use them.
+#Get-Service
+#Get-Process
+#cls
+#Get-Command  "get-service"
+#Get-Command  "ec2"
+#Install-Module -Name AWS.Tools.Installer -Force
+#Update-Help
+#Get-Host | Select-Object Version
+#output
+#=========
+#Version       
+#-------       
+#5.1.18362.1171
+#Enter-PSSession $session
+#winget search Microsoft.PowerShell
+#PowerShell-7.1.2-win-x64.msi
+
+#winget install --name PowerShell --exact
+#winget install --name PowerShell-Preview --exact
+#dotnet tool install --global PowerShell
+#Get-RDCertificate -ConnectionBroker "pv-rdcb02"
+#Get-Alias
+#Get-Help Get-Service
+#PAUSE
+#Get-ExecutionPolicy -List
+#Set-ExecutionPolicy Restricted
+#Get-ExecutionPolicy -List
+#powershell -ExecutionPolicy Bypass -File script.ps1
+#%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -File .\file.ps1
+#Set-ExecutionPolicy -ExecutionPolicy Bypass -File .\firstpracices.ps1
+#get-executionpolicy 
+#Set-ExecutionPolicy RemoteSigned
+#powershell -ExecutionPolicy ByPass -File firstpracices.ps1
+#powershell -v
+#Get-ExecutionPolicy -Scope User
+#Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope LocalMachine
