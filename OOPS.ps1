90c9f969c250409638c5da785de7b0ebeb15a53d
Gitrepository <gitrepository001@gmail.com>
2021-03-03 09:50:13 +0530

    OOPs Related commands second commit

--- OOPS.ps1
+++ OOPS.ps1
@@ -7,3 +7,22 @@ $var1 = 777
 $var2 = 333
 $var3 = $var1 + $var2
 $var3
+#output 1110
+############## Adding Two Strings ####################33
+#Example 2
+$var1 = "Complete"
+$var2 = "Ram"
+$var3 = $var1 + $var2
+$var3
+#Here Getting Error
+
+PS C:\WINDOWS\system32> $myname = "raj" #Declared the variables
+
+PS C:\WINDOWS\system32> Write-Host $myname #here print the variables
+raj
+
+S C:\WINDOWS\system32> $myname.GetType()
+
+IsPublic IsSerial Name                                     BaseType                                          
+-------- -------- ----                                     --------                                          
+True     True     String                                   System.Object                                     
